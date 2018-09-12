3c8d393b881ad871ca687402401aa1ac4c17009f
bbyte <null.0.byte@gmail.com>
2018-09-13 00:54:55 +0300

    fix parser error, refactoring

--- entities/TuneInRadioParser.py
+++ entities/TuneInRadioParser.py
@@ -1,39 +1,40 @@
 __author__ = 'bitybite'
 
 import requests
+import settings
 from bs4 import BeautifulSoup
 
 
 class TuneInRadioParser:
-    __tuneIn_link = 'http://tunein.com'
-    __stations = {
-        'kexp': '/radio/KEXP-FM-903-s32537',
-        'got': '/radio/GotRadio-Indie-Underground-s49685/'
-    }
-    __skipWords = ['US', 'Variety Mix']
-
-    def print_available_stations(self):
-        for x in self.__stations:
+    @staticmethod
+    def print_available_stations():
+        for x in settings.stations:
             print('station ' + x + ' '
-                  + self.__tuneIn_link + self.__stations[x])
+                  + settings.tune_in_link + settings.stations[x])
 
-    def get_skip_words(self):
-        return self.__skipWords
+    @staticmethod
+    def get_skip_words():
+        return settings.skipWords
 
-    def is_station_exists(self, station):
-        if station in self.__stations:
+    @staticmethod
+    def is_station_exists(station):
+        if station in settings.stations:
             return True
         else:
             return False
 
-    def get_song(self, station):
-
+    @staticmethod
+    def get_song(station):
         print('..grabbing info from ' + station)
-        r = requests.get(self.__tuneIn_link + self.__stations[station])
+        r = requests.get(settings.tune_in_link + settings.stations[station])
         if r.status_code != 200:
             print(r.raise_for_status())
 
         parser = BeautifulSoup(r.text, 'html.parser')
-        song = parser.find('li', 'guide-item').find('div', 'content').find('h3').string
+        elements = parser.find_all('p', {'data-testid': 'guideItemTitleSubtitle'})
+        if elements:
+            song = elements[0].text
+        else:
+            raise Exception('Cannot parse page')
 
         return song
