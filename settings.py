9561df2a374feb4f5fb2ff187f83086ef872f949
bbyte <null.0.byte@gmail.com>
2018-09-13 01:00:30 +0300

    update stations list

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ save_to_file = True
 tune_in_link = 'http://tunein.com'
 stations = {
     'kexp': '/radio/KEXP-FM-903-s32537',
-    'got': '/radio/GotRadio-Indie-Underground-s49685/'
+    'got': '/radio/GotRadio-Indie-Underground-s49685/',
+    'indie_radio_canada': '/radio/Indie-Radio-Canada-s215765/',
+    '181fm-blues': '/radio/181FM-True-Blues-s90500/',
+    'classical-portland': '/radio/All-Classical-Portland-899-s31824/',
 }
 skipWords = ['US', 'Variety Mix']
